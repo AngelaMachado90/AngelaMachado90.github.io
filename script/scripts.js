6ef2899777649caa53f03069714d2f5fab9dd93c
Angela Machado <angela.machado02022@gmail.com>
2025-06-26 11:08:52 -0300

    Google Sheet

--- script/scripts.js
+++ script/scripts.js
@@ -95,7 +95,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         try {
             // Enviar para o Google Apps Script - ajuste abaixo para URL do seu deploy correto
-            const response = await fetch('https://script.google.com/macros/s/AKfycbz82xVaa1m_e9Q124OFXdt_GHqd8z1FTxSgJzMYweFgkH3RTyzYAZVc_8x6qbtWB97h/exec', {
+            const response = await fetch('https://script.google.com/macros/s/AKfycbzSwOgUYq8Yjru-5KDC-BOFQ1k2qzaHK2Uc8d8qcR-ZL4PykvhWA-0IDKE_gR3JchWZ/exec', {
                 method: 'POST',
                 body: new FormData(form)
             });
